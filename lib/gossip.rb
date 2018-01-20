577a847d3be057852cf229e4dfc4d0bd0b6b8baa
Joe Jobes <jmrjobes@gmail.com>
2018-01-20 13:56:34 -0700

    Implementing gossiping protocol between clients

--- lib/gossip.rb
+++ lib/gossip.rb
@@ -3,6 +3,7 @@ require 'colorize'
 require 'active_support/time'
 require_relative '../test/coin_test'
 require_relative '../lib/helpers'
+require_relative '../lib/gossip_helper'
 
 PORT, PEER_PORT = ARGV.first(2)
 set :port, PORT
@@ -11,20 +12,34 @@ STATE = ThreadSafe::Hash.new
 update_state(PORT => nil)
 update_state(PEER_PORT => nil)
 
-MOVIES = File.read('./data/movies.txt').map(&:chomp)
+MOVIES = File.readlines('./data/movies.txt').map(&:chomp)
 @favorite_movie = MOVIES.sample
-@version_number = 0
-puts "My favorite movie, now and forever, is #{favorite_movie.green}"
+@version_number = 0 # this is a counter for determining new messages from old messages
+puts "My favorite movie, now and forever, is #{@favorite_movie.green}"
 
 update_state(PORT => [@favorite_movie, @version_number])
 
 every(8.seconds) do
+  puts "You know what screw #{@favorite_movie.yellow} it's so cliche"
+  @favorite_movie = MOVIES.sample
+  @version_number += 1
+  update_state(PORT => [@favorite_movie, @version_number])
+  puts "My new favorite movie is #{@favorite_movie.cyan}"
 end
 
 every(3.seconds) do
-  render_state
+  STATE.dup.each_key do |peer_port|
+    next if peer_port == PORT
+    puts "Gossiping with #{peer_port}.... gossip gossip."
+    their_state = Client.gossip(peer_port, JSON.dump(STATE)) # this will grab the state of a peer network, specifically the state of the PEER_PORT VARIABLE
+    update_state(JSON.parse(their_state)) # this will then update the state so it shows their connected state
+  end
+  render_state # we then render the newly updated state.
 end
 
 # @param state
 post '/gossip' do
+  their_state = params['state'] # this is grabbing the post from the peer_port which is their state
+  update_state(JSON.parse(their_state)) # we are updating the parsed peer_port state
+  JSON.dump(STATE) # then we are dumping our state back in for them to see.
 end
