b6c5dcac527002b3859d4db2001064026586128b
Joe Jobes <jmrjobes@gmail.com>
2018-01-20 16:43:00 -0700

    Implementing chaining of block and blockchain

--- lib/block.rb
+++ lib/block.rb
@@ -2,11 +2,13 @@ require 'colorize'
 require 'digest'
 
 class Block
-  attr_reader :msg
+  NUM_ZEROES = 5 # number of zeroes that trail hash to represent valid nonce
+
+  attr_reader :msg, :previous_block_hash, :block_hash
 
   def initialize(previous_block, msg)
     @msg = msg
-    @previous_block = previous_block.block_hash
+    @previous_block_hash = previous_block.block_hash if !previous_block.nil?
     mine_block!
   end
 
@@ -15,21 +17,21 @@ class Block
   end
 
   def mine_block!
-
+    @nonce = find_nonce
+    @block_hash = hash(@nonce)
   end
 
   private
-
-  NUM_ZEROES = 4 # number of zeroes that trail hash to represent valid nonce
 
   def hash(message) # function to hash the message
     Digest::SHA256.hexdigest(message)
   end
 
-  def find_nonce(message)
+  def find_nonce
     nonce = "HELP I'M TRAPPED IN A NONCE FACTORY"
     count = 0
-    until is_valid_nonce?(nonce, message)
+    until is_valid_nonce?(nonce)
+      print "." if count % 100_000 == 0
       nonce = nonce.next
       count += 1
     end
@@ -37,22 +39,25 @@ class Block
     nonce
   end
 
-  def is_valid_nonce?(nonce, message) # checking to see if hash has trailing zeroes to indicate valid nonce
-    hash(message + nonce).start_with?("0" * NUM_ZEROES)
+  def block_contents
+    [@previous_block_hash, @msg].compact.join
   end
-  
+
+  def is_valid_nonce?(nonce) # checking to see if hash has trailing zeroes to indicate valid nonce
+    hash(block_contents + nonce).start_with?("0" * NUM_ZEROES)
+  end
 end
 
 class BlockChain
-
   attr_reader :blocks
 
   def initialize(msg)
-    @blocks = Block.create_genesis_block(msg)
+    @blocks = [Block.create_genesis_block(msg)]
   end
 
   def add_to_chain(msg)
     @blocks << Block.new(@blocks.last, msg)
+    puts @blocks.last
   end
 
   def valid?
@@ -61,7 +66,6 @@ class BlockChain
   def to_s
     @blocks.map(&:to_s).join("\n")
   end
-
 end
 
 b = BlockChain.new('----Genesis Block----')
