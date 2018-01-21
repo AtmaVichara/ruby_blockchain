969c8d5aeff78220ec833a1903e05134278d8f4e
Joe Jobes <jmrjobes@gmail.com>
2018-01-21 16:07:24 -0700

    Converting pki into class, and methods into class methods.

--- lib/pki.rb
+++ lib/pki.rb
@@ -1,21 +1,23 @@
 require 'openssl'
 require 'base64'
 
-def generate_key_pair
-  key_pair = OpenSSL::PKey::RSA.new(2048)
-  private_key, public_key = key_pair.export, key_pair.public_key.export
-end
+class PKI
+  def self.generate_key_pair
+    key_pair = OpenSSL::PKey::RSA.new(2048)
+    private_key, public_key = key_pair.export, key_pair.public_key.export
+  end
 
-def sign(plaintext, raw_private_key)
-  private_key = OpenSSL::PKey::RSA.new(raw_private_key)
-  Base64.encode64(private_key.private_encrypt(plaintext))
-end
+  def self.sign(plaintext, raw_private_key)
+    private_key = OpenSSL::PKey::RSA.new(raw_private_key)
+    Base64.encode64(private_key.private_encrypt(plaintext))
+  end
 
-def plaintext(cyphertext, raw_public_key)
-  public_key = OpenSSL::PKey::RSA.new(raw_public_key)
-  public_key.public_decrypt(Base64.decode64(cyphertext))
-end
+  def self.plaintext(cyphertext, raw_public_key)
+    public_key = OpenSSL::PKey::RSA.new(raw_public_key)
+    public_key.public_decrypt(Base64.decode64(cyphertext))
+  end
 
-def valid_signature?(message, cyphertext, public_key)
-  message == plaintext(cyphertext, public_key)
+  def self.valid_signature?(message, cyphertext, public_key)
+    message == plaintext(cyphertext, public_key)
+  end
 end
