94dc128a64c21962bbd868f0bbdfe3a18b3c7ce2
Joe Jobes <jmrjobes@gmail.com>
2018-01-21 18:41:37 -0700

    Adding edge case for odd number of ids

--- lib/merkle.rb
+++ lib/merkle.rb
@@ -8,6 +8,7 @@ class MerkleTree
   end
 
   def store_hashed_ids(ids)
+    @hashes << ids[-1] if ids.length % 2 != 0
     if ids.length > 1
       ids.each_slice(2) do |id|
         @hashes << Digest::SHA256.hexdigest(id.join)
